41aa8f488396d4580777d35017bd266bb4b77045
Ambogo2 <ruthambogo.ra@gmail.com>
2023-08-16 07:10:33 -0700

    3-common_elements.py

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    set_common = set()
+    for elements in set_1:
+        if elements in set_2:
+            set_common.add(elements)
+    return(set_common)
