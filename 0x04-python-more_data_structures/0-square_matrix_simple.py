76cb33577ee05d0fe7b1a8594e6d5951895ffa42
Ambogo2 <ruthambogo.ra@gmail.com>
2023-08-16 04:16:38 -0700

    6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    squared_matrix = []
+
+    for row in matrix:
+        square_row = []
+        for num in row:
+            squared_row.append(num ** 2)
+        squared_matrix.append(squared_row)  
+    return(squared_matrix)
