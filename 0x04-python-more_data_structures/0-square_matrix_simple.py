1117a6bac8ef230a4d68c6161b31737c2e2d29ab
Ambogo2 <ruthambogo.ra@gmail.com>
2023-08-16 09:42:31 -0700

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    squared_matrix = []
+def square_matrix_simple(matrix=[]):
+    result_matrix = matrix.copy()
 
-    for row in matrix:
-        square_row = []
-        for num in row:
-            squared_row.append(num ** 2)
-        squared_matrix.append(squared_row)  
-    return(squared_matrix)
+    for i in range(len(matrix)):
+        result_matrix[i] = list(map(lambda x: x ** 2, matrix[i]))
+    return result_matrix
