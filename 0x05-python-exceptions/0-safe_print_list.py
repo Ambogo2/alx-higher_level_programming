8f1d24cdb03b23d194444b08a5221f4a244689c3
Ambogo2 <ruthambogo.ra@gmail.com>
2023-08-28 14:03:25 +0000

    1-safe_print_integer.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        count = 0
-        for item in my_list:
-            if count < x:
-                print(item, end=' ')
-                count += 1
-        print()
-        return count
-    except Exception:
+    count = 0
+    for item in range(x):
+        try:
+            print("{}".format(my_list[i], end='')
+            count += 1
+        except IndexError:
+            break
+        print("")
         return count
