13d8e2995a555dbd4963cffcc42f59efc656848d
Ambogo2 <ruthambogo.ra@gmail.com>
2023-08-30 03:22:52 -0700

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -32,7 +32,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        size.__self = value
+        self.__size = value
 
     def area(self):
         """returns the current area of a square with # character."""
