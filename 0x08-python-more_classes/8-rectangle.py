d1bfddcc1b48705ed0caa3edd0ea7ba2a9a30f45
Ambogo2 <ruthambogo.ra@gmail.com>
2023-11-27 14:59:06 +0300

    8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -79,8 +79,22 @@ class Rectangle:
 
     def __del__(self):
         """
-        Prints the message "Bye rectangle..." when an instance of Rectangle is deleted.
+        Prints the message "Bye rectangle...".
         Decrements number_of_instances during each instance deletion.
         """
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """
+        returns the biggest rectangle based on the area.
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_2
+        else:
+            return rect_2
