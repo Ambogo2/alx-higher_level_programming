994e32f5295797cdd7f66ddbd6cc52eed24398a3
Ambogo2 <ruthambogo.ra@gmail.com>
2023-11-27 15:54:06 +0300

    commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,19 +18,6 @@ class Rectangle():
         self.__width = width
 
     @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-
-        self.__width = value
-
-    @property
     def height(self):
         """
         retrives attributes.
