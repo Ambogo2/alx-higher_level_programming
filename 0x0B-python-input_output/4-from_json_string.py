146acbda855bfa9b7a576dc457daf904f6b8f559
Ambogo2 <ruthambogo.ra@gmail.com>
2023-12-05 17:40:40 +0300

    commit

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -5,7 +5,7 @@ import json
 
 def from_json_string(my_str):
     """
-    from_json_string- returns an object represented by a JSON string 
+    from_json_string- returns an object represented by a JSON string
     args:
         my_str:string
     """
