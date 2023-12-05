723766592c2f29189a02e1baf7432c9bf141b0a1
Ambogo2 <ruthambogo.ra@gmail.com>
2023-12-05 12:10:07 +0300

    reads a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A module containing read_file function.]"""
+
+
+def read_file(filename=""):
+    """
+    read_file - reads a file
+    filename:file to be read
+    """
+    with open("my_file_0.txt", "r") as file:
+        content = file.read()
+        print(content)
