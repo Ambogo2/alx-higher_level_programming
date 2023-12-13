1ac922a13b8bd9cd6df6a3c786ea1f65944dd63c
Ambogo2 <ruthambogo.ra@gmail.com>
2023-12-13 21:54:10 +0300

    commit

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ def read_file(filename=""):
     read_file - reads a file
     filename:file to be read
     """
-    with open("my_file_0.txt", "r", encoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
