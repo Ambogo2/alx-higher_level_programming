c20a06ce103de448db9c00717c250e2331d182df
Ambogo2 <ruthambogo.ra@gmail.com>
2024-01-09 20:43:36 +0300

    9-main.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -21,10 +21,10 @@ class Student():
         Returns:
                A dictionary containing the attributes.
         """
-    student_dict = {
-        'first_name':self.first_name,
-        'last_name':self.last_name,
-        'age':self.age
+        student_dict = {
+            'first_name':self.first_name,
+            'last_name':self.last_name,
+            'age':self.age
 
-    }
-    return student_dict
+        }
+        return student_dict
