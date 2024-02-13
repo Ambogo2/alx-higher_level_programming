30f50c73449fbb7429ef275402b048c593529fe1
Ambogo2 <ruthambogo.ra@gmail.com>
2024-02-13 12:46:40 +0300

    table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS second_table(
     score INT
 );
 INSERT INTO  second_table (id, name, score)
-VALUES (1,  'JohnBest', 10);
+VALUES (1,  'John', 10);
 INSERT INTO second_table (id, name, sore)
 VALUES (2, 'Alex', 3);
 INSERT INTO second_table (id, name, score)
