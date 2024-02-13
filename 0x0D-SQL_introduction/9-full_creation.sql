188159ce765a05ff3a9c619599fc25e853e7def2
Ambogo2 <ruthambogo.ra@gmail.com>
2024-02-13 12:35:34 +0300

    added

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- creates a table second_table in the database hbtn_0c_0 
+
+CREATE TABLE IF NOT EXISTS second_table(
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO first_table VALUES {1,  JohnBest, 10};
+INSERT INTO first_table VALUES {2, Alex, 3};
+INSERT INTO first_table VALUES {3, Bob, 14};
+INSERT INTO first_table VALUES {4, George, 8};
