95294f289302baa7c1bcc4b4a2292594b1044437
Ambogo2 <ruthambogo.ra@gmail.com>
2024-02-15 11:06:40 +0300

    commit

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,11 +2,12 @@
 
 SELECT tv_shows.title
 FROM tv_shows
-WHERE tv_shows.id NOT IN(
+WHERE tv_shows.id NOT IN (
     SELECT tv_shows.id
     FROM tv_shows
+    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
     JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-    JOIN tv_shows_genres ON tv_shows.id = tv_shows_genres.genre_id
     WHERE tv_genres.name = 'Comedy'
 )
 ORDER BY tv_shows.title ASC;
+
