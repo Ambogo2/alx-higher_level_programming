6c2d895ca77c85ce2bc554d7889e49c428a3af75
Ambogo2 <ruthambogo.ra@gmail.com>
2024-02-15 10:55:57 +0300

    change

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,8 +2,8 @@
 
 SELECT tv_genres.name, sum(tv_shows_ratings.rate) AS rating_sum
 FROM  tv_genres
-JOIN tv_shows_genres 
-ON tv_genres.id = tv_shows_genres.genres_id
+JOIN tv_show_genres 
+ON tv_genres.id = tv_show_genres.genres_id
 JOIN tv_shows 
 ON tv_show_genres.show_id = tv_shows.id
 JOIN tv_show_ratings 
