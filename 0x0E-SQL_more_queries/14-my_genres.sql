e362fd5076879cdbfc20ed860130ac520db908a4
Ambogo2 <ruthambogo.ra@gmail.com>
2024-02-15 10:51:25 +0300

    changed

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,7 +3,7 @@
 SELECT tv_genres.name
 FROM tv_shows
 JOIN tv_show_genres
-ON tv_shows.id = tv_shows_genres.show_id
+ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres.id
 ON tv_show_genres.id = tv_show_genres.id 
 WHERE tv_shows.title = 'Dexter'
