b260a9df658498b0e47fcc8735ced8dded50beae
Ambogo2 <ruthambogo.ra@gmail.com>
2024-02-15 10:50:12 +0300

    commit

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -2,7 +2,7 @@
 
 SELECT tv_genres.name
 FROM tv_shows
-JOIN tv_shows_genres
+JOIN tv_show_genres
 ON tv_shows.id = tv_shows_genres.show_id
 JOIN tv_genres.id
 ON tv_show_genres.id = tv_show_genres.id 
