2b6509d47a593421508746ac92ddab3cc3dfc5ef
Ambogo2 <ruthambogo.ra@gmail.com>
2024-04-10 22:04:51 +0300

    square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const Rectangle = require('./5-square');
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
+  }
+
+  ycharPrint(c) {
+    if (c === undefined) {
+      c = "X";
+    }
+    for (let i = 0; i < this.height; i++) {
+      let row = "";
+      for (let j = 0; j < this.width; j++) {
+        row += c;
+      }
+      console.log(row);    }
+  }
+}
