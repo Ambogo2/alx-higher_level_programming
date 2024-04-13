40c94f5ec0671d28819201656ac8bcfe7801c38b
Ambogo2 <ruthambogo.ra@gmail.com>
2024-04-13 21:35:21 +0300

    model_city.py

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A module that connects to the database"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+import sys
+from model_state import Base, State
+
+class City(Base):
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey="states.id")
