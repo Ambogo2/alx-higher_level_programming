0ca3afb7f8cea21c2e43205b13de07e18e920f73
Ambogo2 <ruthambogo.ra@gmail.com>
2024-04-13 21:52:07 +0300

    13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -7,8 +7,7 @@ from sqlalchemy import create_engine
 import sys
 
 if __name__ == "__main__":
-    connection_string = "mysql+mysqldb://{}:{}@localhost/{}"
-    .format(sys.argv[1], sys.argv[2], sys.argv[3])
+    connection_string = "mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3])
     engine = create_engine(connection_string, pool_pre_ping=True)
 
     # Create session maker
