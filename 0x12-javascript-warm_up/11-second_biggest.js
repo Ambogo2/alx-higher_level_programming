3bc15fd537438515a1f33139ce856a67772122b2
Ambogo2 <ruthambogo.ra@gmail.com>
2024-06-18 11:45:33 +0300

    second_biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv.length === 2) {
+  console.log(0);
+} else if (process.argv.length === 3) {
+  console.log(0);
+} else {
+  console.log(`${parseInt(process.argv[2])}`);
+}
