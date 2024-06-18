5f0dd9b5ff75ac5be2e7067c8b01bbbe5e16096f
Ambogo2 <ruthambogo.ra@gmail.com>
2024-06-18 09:07:55 +0300

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,11 +3,9 @@
 function fact (n) {
   if (isNaN(n)) {
     return 1;
-  }
-  else if (n === 0) {
+  } else if (n === 0) {
     return 1;
-  }
-  else {
+  } else {
     return n * fact(n - 1);
   }
 }
