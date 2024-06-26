551ec389a95a79babc8ed21b516b35a2783224af
Ambogo2 <ruthambogo.ra@gmail.com>
2024-06-26 20:48:49 +0300

    first state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""connects a python script to a database"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+
+if __name__ == "__main__":
+    connection_string = "mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3])
+    engine = create_engine(connection_string, pool_pre_ping=True)
+
+    my_session_maker = sessionmaker(bind=engine)
+
+    # instance of session
+    my_session = my_session_maker()
+
+    states = my_session.query(State).order_by(State.id).first()
+
+    if states is None:
+        print ("Nothing")
+    else:
+        print("{}:{}".format(states.id, states.name))
+
+    my_session.close()
