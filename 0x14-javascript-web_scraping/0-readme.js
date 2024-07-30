073e188f489cc9b04ca6d4ced390b831a85efb82
Ambogo2 <ruthambogo.ra@gmail.com>
2024-07-30 14:39:55 +0300

    Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// import module
+const fs = require("fs");
+
+const filepath = process.argv[2];
+
+// read the file
+fs.readFile(filepath, "utf8", (err, data) => {
+
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+
+  console.log(data);
+});
