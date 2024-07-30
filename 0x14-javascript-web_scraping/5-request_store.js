3e1387ccbdccd14d6977017154e7f4bcc5c4d48b
Ambogo2 <ruthambogo.ra@gmail.com>
2024-07-30 21:59:09 +0300

    5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require("request");
+const fs = require("fs");
+
+const apiUrl = process.argv[2];
+const filepath = process.argv[3];
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(filepath, body, "utf8", function (error, data) {
+      if (error) {
+        console.error(error);
+      }
+    });
+  }
+});  
