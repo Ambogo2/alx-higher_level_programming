03431387dad9f470f7eb34cbfd171eddf5c27755
Ambogo2 <ruthambogo.ra@gmail.com>
2024-07-30 23:14:17 +0300

    titles

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 // Import module
-const request = require("request");
+const request = require('request');
 
 const movieId = process.argv[2];
 
-const apiUrl = "https://swapi-api.alx-tools.com/api/films/" + movieId;
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
 // making HTTP GET request to the API
 request(apiUrl, function (error, response, body) {
