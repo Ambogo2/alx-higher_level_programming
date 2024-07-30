252aff52abd93f700ee9c6ca824eabe13a42669a
Ambogo2 <ruthambogo.ra@gmail.com>
2024-07-30 14:54:50 +0300

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+// Import module
+const request = require("request");
+
+const url = process.argv[2];
+
+// Make an HTTP GET request to the URL
+request(url, function (error, response, body) {
+  if (error) {
+    console.error("Error:", error);
+  } else {
+    console.log("Code:", response.statusCode);
+  }
+});
