5902d7cb27227498eaf2d6aaccc93da167ba7292
Ambogo2 <ruthambogo.ra@gmail.com>
2024-07-30 23:03:36 +0300

    loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const request = require("request");
-const fs = require("fs");
+const request = require('request');
+const fs = require('fs');
 
 const apiUrl = process.argv[2];
 const filepath = process.argv[3];
@@ -10,10 +10,10 @@ request(apiUrl, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
-    fs.writeFile(filepath, body, "utf8", function (error, data) {
+    fs.writeFile(filepath, body, 'utf8', function (error, data) {
       if (error) {
         console.error(error);
       }
     });
   }
-});  
+});
