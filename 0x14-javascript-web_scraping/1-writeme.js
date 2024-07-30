c0827de908d07203cd0f10a577169581cc436121
Ambogo2 <ruthambogo.ra@gmail.com>
2024-07-30 23:48:52 +0300

    write file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
 
 // import module
-const fs = require("fs");
+const fs = require('fs');
 
 const filepath = process.argv[2];
 
 const content = process.argv[3];
 
 // read the file
-fs.writeFileFile(filepath, content, "utf8", err => {
-
+fs.writeFile(filepath, content, 'utf8', err => {
   if (err) {
-    console.error("Error writing file:", err);
-    return;
+    console.error('Error writing file:', err);
   }
 });
